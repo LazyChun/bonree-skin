d5b331ce967b09fd21e072924eca9f8c91722aa2
LazyChun <1299362885@qq.com>
2021-06-07 02:35:23 +0800

    init all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App"
+
+const appStyle = {
+  maxWidth: 1300,
+  minHeight: 574
+};
+
+const App = () => {
+  return <div style={appStyle}>Welcome to LazyChun !</div>;
+};
+
+export default App;
